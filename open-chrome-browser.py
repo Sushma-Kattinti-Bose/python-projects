317181618ac0e3b873d594adcbd5aae58e76c293
Sushma Kattinti <sushma.kattinti@fabianware.io>
2017-03-25 22:33:24 -0400

    Change to phantomjs

--- open-chrome-browser.py
+++ open-chrome-browser.py
@@ -1,8 +1,9 @@
 import time
 from selenium import webdriver
 
-driver = webdriver.Chrome()  # Optional argument, if not specified will search path.
-driver.get('http://www.google.com/xhtml');
+driver = webdriver.PhantomJS()  # Optional argument, if not specified will search path.
+driver.set_window_size(1120, 550)
+driver.get('http://www.google.com/xhtml')
 time.sleep(5) # Let the user actually see something!
 search_box = driver.find_element_by_name('q')
 search_box.send_keys('ChromeDriver')
