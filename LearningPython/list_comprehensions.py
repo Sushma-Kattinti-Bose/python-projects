2a184e9d74a4ad91cef7f0149e762588b753e9c1
Sushma Kattinti <sushma.kattinti@fabianware.io>
2017-04-03 23:03:50 -0500

    Python Basics

--- LearningPython/list_comprehensions.py
+++ LearningPython/list_comprehensions.py
@@ -1,5 +1,5 @@
 def list_comprehensions():
-
+    
     grades = [76, 89, 90, 66]
     print(grades)
     grades = [grade + 5 for grade in grades]
@@ -8,6 +8,14 @@ def list_comprehensions():
     print(words)
     words = [word.lower() for word in words]
     print(words)
+    grades = [grade.rstrip() for grade in open('grades.txt')]
+    print(grades)
+    sent = "now is the time for all good people to come to the aid "
+    sent += "of their party"
+    words = sent.split(' ')
+    wlen = [(word, len(word)) for word in words]
+    for i in wlen:
+        print(i)
 
 if __name__ == "__main__":
     list_comprehensions()
