1e14b61abadd04606f24cdc5ac7489bfad24c186
Sushma Kattinti <sushma.kattinti@fabianware.io>
2017-04-07 14:38:45 -0500

    Python Basics

---
+++ LearningPython/test_modulesphere.py
@@ -0,0 +1,7 @@
+# from modules_sphere import *
+import modules_sphere
+
+print("Enter the radius of the spehere:")
+radius = int(input())
+print("The area is " + str(modules_sphere.area(radius)))
+print("The volume is " + str(modules_sphere.volume(radius)))
