18063af5571c2c636823fe1a16a9772b51c56e0d
Sushma Kattinti <sushma.kattinti@fabianware.io>
2017-04-07 23:39:37 -0500

    Python Basics

---
+++ LearningPython/name.py
@@ -0,0 +1,8 @@
+class Name:
+    # constructor methods - creating instance of the class 
+    def __init__(self, first, middle, last):
+        self.first = first
+        self.middle = middle 
+        self.last = last
+aName = Name('Robert', 'Michael', 'Smith')
+yourName = Name('Sushma', 'S', 'Kattinti')
---
+++ LearningPython/name1.py
@@ -0,0 +1,22 @@
+class Name:
+    # constructor methods - creating instance of the class 
+    def __init__(self, first, middle, last):
+        self.first = first
+        self.middle = middle 
+        self.last = last
+
+    # to-string method
+    def __str__(self):
+        return self.first + ' ' + self.middle + ' ' + self.last
+    
+    def lastFirst(self):
+        return self.last + ' ' + self.first + ' ' + self.middle
+
+    def initials(self):
+        return self.first[0] + ' ' + self.middle[0] + ' ' + self.last[0]
+
+
+aName = Name('Robert', 'Michael', 'Smith')
+print('aName is ' + str(aName))
+print(aName.lastFirst())
+print(aName.initials())
