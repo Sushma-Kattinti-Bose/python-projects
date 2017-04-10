51e419ae77b0184c87f61cfe927d88798c0ca230
Sushma Kattinti <sushma.kattinti@fabianware.io>
2017-04-10 16:36:03 -0400

    Python Basics

--- LearningPython/except_statement1.py
+++ LearningPython/except_statement1.py
@@ -1,24 +1,37 @@
-def calulator(op1, op2, op):
-    if op == '+':
-        return op1 + op2
-    elif op == '-':
-        return op1 - op2
-    elif op == '*':
-        return op1 * op2
-    elif op == '/':
-        return op1 / op2
-cont = 'y'
-while cont != 'n':
-    print("Enter the first number")
-    num1 = int(input())
-    print("Enter the Second number")
-    num2 = int(input())
-    print("Enter the Operation")
-    op = input()
-    if op == '/' and num2 == 0:
-        print("Cannot divide by zero")
-        continue
-    else:
-        print(calulator(num1, num2, op))
-    print("Do you want to continue(y/n)?")
-    cont = input()
+import os
+
+print("Enter the file name to open")
+name = input()
+while not os.path.isfile(name):
+    print("File does not exist")
+    print("Enter the file name to open: ")
+    name = input()
+file = open(name, 'r')
+for line in file:
+    print(line, end='')
+
+# def calulator(op1, op2, op):
+#     if op == '+':
+#         return op1 + op2
+#     elif op == '-':
+#         return op1 - op2
+#     elif op == '*':
+#         return op1 * op2
+#     elif op == '/':
+#         return op1 / op2
+
+# cont = 'y'
+# while cont != 'n':
+#     print("Enter the first number")
+#     num1 = int(input())
+#     print("Enter the Second number")
+#     num2 = int(input())
+#     print("Enter the Operation")
+#     op = input()
+#     if op == '/' and num2 == 0:
+#         print("Cannot divide by zero")
+#         continue
+#     else:
+#         print(calulator(num1, num2, op))
+#     print("Do you want to continue(y/n)?")
+#     cont = input()
